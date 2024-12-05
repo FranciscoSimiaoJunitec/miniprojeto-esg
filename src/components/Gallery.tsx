2ba5624c471306a371c8ad7646fa90daaa3f9fd1
FranciscoSimiaoJunitec <francisco.simiai@junitec.pt>
2024-12-05 14:33:38 +0000

    Update image sources in Gallery component

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -5,30 +5,30 @@ import './Gallery.css';
 
 const Gallery: React.FC = () => {
   const images = [
-    `${process.env.PUBLIC_URL}/1.png`,
-    `${process.env.PUBLIC_URL}/2.png`,
-    `${process.env.PUBLIC_URL}/3.png`,
-    `${process.env.PUBLIC_URL}/4.png`,
-    `${process.env.PUBLIC_URL}/5.png`,
-    `${process.env.PUBLIC_URL}/6.png`,
-    `${process.env.PUBLIC_URL}/7.png`,
-    `${process.env.PUBLIC_URL}/8.png`,
-    `${process.env.PUBLIC_URL}/9.png`,
-    `${process.env.PUBLIC_URL}/10.png`,
-    `${process.env.PUBLIC_URL}/11.png`,
-    `${process.env.PUBLIC_URL}/12.png`,
-    `${process.env.PUBLIC_URL}/13.png`,
-    `${process.env.PUBLIC_URL}/14.png`,
-    `${process.env.PUBLIC_URL}/15.png`,
-    `${process.env.PUBLIC_URL}/16.png`,
-    `${process.env.PUBLIC_URL}/17.png`,
-    `${process.env.PUBLIC_URL}/18.png`,
-    `${process.env.PUBLIC_URL}/19.png`,
-    `${process.env.PUBLIC_URL}/20.png`,
-    `${process.env.PUBLIC_URL}/21.png`,
-    `${process.env.PUBLIC_URL}/22.png`,
-    `${process.env.PUBLIC_URL}/23.png`,
-    `${process.env.PUBLIC_URL}/24.png`,
+    'https://i.ibb.co/7p7pkSS/Prancheta-18.png',
+    'https://i.ibb.co/nQGT7sJ/Prancheta-20-1.png',
+    'https://i.ibb.co/7KGWBm6/Prancheta-20.png',
+    'https://i.ibb.co/NNqFRQ7/Prancheta-21.png',
+    'https://i.ibb.co/tmcWY9j/Prancheta-22.png',
+    'https://i.ibb.co/qk2D83g/Prancheta-24.png',
+    'https://i.ibb.co/y8q8TzV/Prancheta-25.png',
+    'https://i.ibb.co/6RqWV6j/Prancheta-26.png',
+    'https://i.ibb.co/93Xm0YJ/Prancheta-28-1.png',
+    'https://i.ibb.co/4F2ZS3Q/Prancheta-28.png',
+    'https://i.ibb.co/PDZ1r7f/Prancheta-29.png',
+    'https://i.ibb.co/LrVsC8B/Prancheta-30.png',
+    'https://i.ibb.co/198Z0r8/Prancheta-31.png',
+    'https://i.ibb.co/rkp3jwt/Prancheta-32.png',
+    'https://i.ibb.co/0qVVNTr/Prancheta-33.png',
+    'https://i.ibb.co/6wQZ5kM/Prancheta-35-1.png',
+    'https://i.ibb.co/F66jWW9/Prancheta-35.png',
+    'https://i.ibb.co/t2v1cZ1/Prancheta-36.png',
+    'https://i.ibb.co/SsFDPBd/Prancheta-37.png',
+    'https://i.ibb.co/pQzshC6/Prancheta-39.png',
+    'https://i.ibb.co/QQDW2bL/Prancheta-40.png',
+    'https://i.ibb.co/rd39yjZ/Prancheta-41.png',
+    'https://i.ibb.co/J2XzD8Z/Prancheta-43.png',
+    'https://i.ibb.co/Z2w5Hpb/Prancheta-44.png',
   ];
 
   useEffect(() => {
@@ -62,6 +62,8 @@ const Gallery: React.FC = () => {
       <Carousel 
         responsive={responsive} 
         infinite 
+        autoPlay 
+        autoPlaySpeed={3000} 
         keyBoardControl 
         showDots
       >
