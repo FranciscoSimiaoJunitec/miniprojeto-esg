6faff5da8823b3ee9fef4c56a0e2250748fdbbe3
FranciscoSimiaoJunitec <francisco.simiai@junitec.pt>
2026-02-26 09:36:48 +0000

    tree fixes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
     plugins: [react(), viteTsconfigPaths()],
-    base: '/miniprojeto-esg/',
+    base: '/',
     server: {
         port: 3000,
         open: true,
